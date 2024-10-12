514a2c13b8377695f04aceea0f585572cfd9f44d
Varshil Shah <varshilshah.in@gmail.com>
2024-10-12 11:37:18 +0530

    Add logout button in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:streamscape/routes.dart';
+import 'package:streamscape/services/auth_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Home Screen",
-          style: TextStyle(fontSize: 18),
+    final AuthService authService = AuthService();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "Home Screen",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+                onPressed: () {
+                  authService.signout(context);
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    Routes.signin,
+                    (route) => false,
+                  );
+                },
+                child: const Text(
+                  "Logout",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                )),
+          ],
         ),
       ),
     );
